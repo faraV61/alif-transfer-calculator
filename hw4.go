6dd4e8c29531ec39f40b9a6e60def357a7be0c0f
fara <iskfaran@gmail.com>
2026-04-17 23:49:33 +0500

    feat: добавлена карточка товара с рассрочкой

--- hw4.go
+++ hw4.go
@@ -3,16 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var sender string = "Ali"
-	var receiver string = "Vali"
-	var amount int = 100000
-	commission := amount * 1 / 100
-	total := amount + commission
-	fmt.Println("Чек Перевода:")
-	fmt.Printf("Отправитель: %s\n", sender)
-	fmt.Printf("Получатель: %s\n", receiver)
-	fmt.Printf("Сумма перевода: %d сум\n", amount)
-	fmt.Printf("Комиссия (1%%): %d сум\n", commission)
-	fmt.Printf("Итого: %d сум\n", total)
-	
+
+	//var sender string = "Ali"
+	//var receiver string = "Vali"
+	//var amount int = 100000
+	//commission := amount * 1 / 100
+	//total := amount + commission
+	//fmt.Println("Чек Перевода:")
+	//fmt.Printf("Отправитель: %s\n", sender)
+	//fmt.Printf("Получатель: %s\n", receiver)
+	//fmt.Printf("Сумма перевода: %d сум\n", amount)
+	//fmt.Printf("Комиссия (1%%): %d сум\n", commission)
+	//fmt.Printf("Итого: %d сум\n", total)
+
+	var name string = "iPhone 15 Pro"
+	var brand string = "Apple"
+	var price int64 = 12_990_000
+	var stock bool = true
+	months := 12
+	monthly := price / int64(months)
+	fmt.Println("===== Alifshop =====")
+	fmt.Printf("Товар: %s\n", name)
+	fmt.Printf("Бренд: %s\n", brand)
+	fmt.Printf("Цена: %d сум\n", price)
+	fmt.Printf("В наличии: %t\n", stock)
+	fmt.Printf("Рассрочка: %d мес → %d сум/мес\n", months, monthly)
 }
